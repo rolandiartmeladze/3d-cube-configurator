4ec4893231a28edd6b437025b938d150a6e59a21
Roland Artmeladze <rartmeladze@gmail.com>
2025-07-09 15:30:51 -0700

    feat: I installed Tailwind CSS for better performance and to create the most dynamic interface.

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  content: [
+    "./index.html",
+    "./main.js",
+    "./core/**/*.js",
+    "./objects/**/*.js",
+    "./ui/**/*.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#A50000",
+        secondary: "#393837",
+        burlywood: "#deb887",
+      },
+    },
+  },
+  plugins: [],
+};
