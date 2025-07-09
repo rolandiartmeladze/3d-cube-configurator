87bf2394a429d4e4f3590a0a680372398c37128d
Roland Artmeladze <rartmeladze@gmail.com>
2025-07-09 15:32:07 -0700

    ref: I updated relevant code details for better presentation and optimization.

--- main.js
+++ main.js
@@ -4,10 +4,24 @@ import { renderer } from './core/renderer.js';
 import { controls } from './core/controls.js';
 import { cube, updateCubeMaterial } from './objects/cube.js';
 import { setupTextureSwitcher } from './ui/textureSwitcher.js';
+import './global.css';
 
 scene.add(cube);
 
-const canvas = document.querySelector('#webgl');
+function resizeRenderer() {
+  const canvas = document.querySelector('#webgl');
+  const { width, height } = canvas.getBoundingClientRect();
+
+  renderer.setSize(width, height, false);
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+}
+
+resizeRenderer();
+
+window.addEventListener('resize', resizeRenderer);
 
 function animate() {
   cube.rotation.x += 0.01;
@@ -17,13 +31,5 @@ function animate() {
   renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
-
-window.addEventListener('resize', () => {
-  const width = canvas.clientWidth;
-  const height = canvas.clientHeight;
-  camera.aspect = width / height;
-  camera.updateProjectionMatrix();
-  renderer.setSize(width, height, false);
-});
 
 setupTextureSwitcher(updateCubeMaterial);
