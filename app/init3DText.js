15ed8eb8b7a54c1e938707bc8ce58e74b8059331
Roland Artmeladze <rartmeladze@gmail.com>
2025-07-13 04:50:24 -0700

    ref:  Updated the element to display a custom text storyboard.

--- app/init3DText.js
+++ app/init3DText.js
@@ -3,6 +3,7 @@ import { createCamera } from '../core/camera.js';
 import { createRenderer } from '../core/renderer.js';
 import { createControls } from '../core/controls.js';
 import { sphere } from '../objects/sphere.js';
+import { Text3D } from '../objects/text.js';
 
 export function initText() {
     const canvas = document.querySelector('#text');
@@ -13,6 +14,16 @@ export function initText() {
 
     const getSphere = sphere({ hue: 0.55 });
     scene.add(getSphere);
+
+    let textGroup = null;
+
+    Text3D({
+        message: 'Roland',
+        onLoad: (loadedGroup) => {
+            textGroup = loadedGroup;
+            scene.add(textGroup);
+        }
+    });
 
     function animate(time) {
         getSphere.userData.update(time);
