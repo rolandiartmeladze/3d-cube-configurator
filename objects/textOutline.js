1f1777d2baec0e68bfcd13daaac4eacaa3a852db
Roland Artmeladze <rartmeladze@gmail.com>
2025-07-15 02:29:54 -0700

    feat: generate text shapes and initialize LineMaterial for outline rendering

--- objects/textOutline.js
+++ objects/textOutline.js
@@ -2,3 +2,19 @@ import * as THREE from 'three';
 import { Line2 } from 'three/examples/jsm/lines/Line2.js';
 import { LineMaterial } from 'three/examples/jsm/lines/LineMaterial.js';
 import { LineGeometry } from 'three/examples/jsm/lines/LineGeometry.js';
+
+export function createTextOutlines({ font, message = "Roland", size = 1 }) {
+
+    const group = new THREE.Group();
+    const shapes = font.generateShapes(message, size);
+
+    const material = new LineMaterial({
+        color: 0xffffff,
+        linewidth: 0.005,
+        dashed: true,
+        dashSize: 1,
+        gapSize: 1,
+    })
+    const strokeMeshes = [];
+    
+};
