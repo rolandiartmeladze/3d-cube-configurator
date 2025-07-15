8f8a4c0bb06ca4e3f38b42fea708b7cb7fccf61a
Roland Artmeladze <rartmeladze@gmail.com>
2025-07-15 03:10:12 -0700

    feat: implement initial dashed outline generation and animation logic for 3D text

--- objects/textOutline.js
+++ objects/textOutline.js
@@ -16,5 +16,27 @@ export function createTextOutlines({ font, message = "Roland", size = 1 }) {
         gapSize: 1,
     })
     const strokeMeshes = [];
-    
+
+    shapes.forEach((shape, index) => {
+        const points = shape.getPoints();
+        const positions = [];
+
+        points.forEach(p=> positions.push(p.x, p.y, 0));
+
+        const lineGeo = new LineGeometry();
+        lineGeo.setPositions(positions);
+
+        const line = new Line2(lineGeo, material);
+        line.computeLineDistances();
+
+        line.userData.totalLength = shape.getLength();
+        line.userData.offsetFactop = index * 0.1;
+
+        group.add(line);
+        strokeMeshes.push(line);
+
+    });
+
+    return group;
+
 };
