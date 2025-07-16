c4f6076d6d8116d2a7000ff5fd1cc66da26479b4
Roland Artmeladze <rartmeladze@gmail.com>
2025-07-16 15:05:23 -0700

    feat: I made changes to the code. I added a simple button for navigation on small devices to simplify navigation and better represent user input tools with minimal visual effects for further development / I updated the code to reflect the changes.

--- main.js
+++ main.js
@@ -2,5 +2,17 @@ import './global.css';
 import { initCube } from './app/initCube.js';
 import { initText } from './app/init3DText.js';
 
+function menuOpen() {
+  const menuBtn = document.querySelector("#hamburg-menu");
+
+  if (!menuBtn) return;
+
+  menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("hamburg-open");
+  });
+}
+
+menuOpen();
+
 initCube();
 initText();
