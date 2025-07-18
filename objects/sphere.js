ba72f1599ebd1829093e1195c5e8dcf2dc54e408
Roland Artmeladze <rartmeladze@gmail.com>
2025-07-18 06:46:31 -0700

    feat: Customize My Elementor background color option for the user using the input slider. The user can select a color or create a dynamic animation of the background.

--- objects/sphere.js
+++ objects/sphere.js
@@ -5,6 +5,8 @@ import * as THREE from "three";
 
 // Create a background sphere using IcosahedronGeometry
 export function sphere({ hue = 0.565, lightnessMult = 0.015 } = {}) {
+  const startAnimation = document.getElementById("reset");
+
   const sphereGeo = new THREE.IcosahedronGeometry(3.5, 4);
   const initialPositions = sphereGeo.attributes.position.array.slice();
 
@@ -30,7 +32,38 @@ export function sphere({ hue = 0.565, lightnessMult = 0.015 } = {}) {
 
   const mesh = new THREE.Mesh(sphereGeo, sphereMat);
 
+  let currentHue = hue;
+  let isAnimating = true;
+
+  startAnimation.addEventListener("click", () => {
+    startAnimation.classList.remove("bg-red-600/60");
+      startAnimation.classList.add("bg-gray-600/60");    
+      isAnimating = true;
+  });
+
+  //stop animation and apply a final hue with gradient
+  mesh.userData.setHue = (newHue) => {
+    currentHue = newHue;
+    isAnimating = false;
+    const colorAttr = mesh.geometry.attributes.color;
+    const colorArr = colorAttr.array;
+
+    for (let i = 0; i < len; i++) {
+      const z = -sphereGeo.attributes.position.getZ(i);
+      const { r, g, b } = new THREE.Color().setHSL(currentHue, 1, z * lightnessMult);
+      const j = i * 3;
+      colorArr[j] = r;
+      colorArr[j + 1] = g;
+      colorArr[j + 2] = b;
+    }
+
+    colorAttr.needsUpdate = true;
+  };
+
+  //animation (only runs if isAnimating = true)
   mesh.userData.update = (time) => {
+    if (!isAnimating) return;
+
     const posAttr = mesh.geometry.attributes.position;
     const arr = posAttr.array;
     const colorAttr = mesh.geometry.attributes.color;
@@ -46,7 +79,7 @@ export function sphere({ hue = 0.565, lightnessMult = 0.015 } = {}) {
     }
     posAttr.needsUpdate = true;
 
-    const animatedHue = (hue + time * 0.0001) % 1;
+    const animatedHue = (currentHue + time * 0.0001) % 1;
     for (let i = 0; i < len; i++) {
       const z = -sphereGeo.attributes.position.getZ(i);
       const { r, g, b } = new THREE.Color().setHSL(animatedHue, 1, z * lightnessMult);
